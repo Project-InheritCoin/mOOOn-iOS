265ecff71591c1f59a3f15f33ee56339b922d133
조현호 <hh.cho>
2018-01-14 21:16:33 +0900

    Initial Commit

---
+++ mOOOnWallet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mOOOnWallet
+//
+//  Created by  조현호 on 2018. 1. 14..
+//  Copyright © 2018년 mOOOn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
